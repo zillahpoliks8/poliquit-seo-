99b3e242072e6df4622f1903d21125e9f803a4ec
zillah paola <poliquitzillahit1b@gmail.com>
2017-07-26 15:55:39 +0800

    commit

--- server.js
+++ server.js
@@ -1,38 +1,41 @@
-//require modules
+
 var express = require('express');
 var path = require('path');
-//instantiate express
+
 var app = express();
-//set port
+
 app.set('port', (process.env.PORT || 5000));
-//use static files
+
 app.use(express.static(path.join(__dirname, 'public')));
-//express routes
+
 app.get('/', function(req, res){
-  res.sendFile(path.join(__dirname, 'views/index.html'));
+  res.sendFile(path.join(__dirname, 'public/index.html'));
 });
-app.get('/about', function(req, res){
-  res.sendFile(path.join(__dirname, 'views/about.html'));
- });
-app.get('/codes', function(req, res){
-  res.sendFile(path.join(__dirname, 'views/codes.html'));
- });
-app.get('/contact', function(req, res){
-  res.sendFile(path.join(__dirname, 'views/contact.html'));
- });
-app.get('/gallery', function(req, res){
-  res.sendFile(path.join(__dirname, 'views/gallery.html'));
- });
-app.get('/icons', function(req, res){
-  res.sendFile(path.join(__dirname, 'views/icons.html'));
- });
-app.get('/single', function(req, res){
-  res.sendFile(path.join(__dirname, 'views/single.html'));
- });
-app.get('/codes2', function(req, res){
-  res.sendFile(path.join(__dirname, 'views/codes2.html'));
- });
-//express server listen
+
+app.get('/', function(req, res){
+  res.sendFile(path.join(__dirname, 'public/HW1.html'));
+});
+
+app.get('/', function(req, res){
+  res.sendFile(path.join(__dirname, 'public/HW2.html'));
+});
+
+app.get('/', function(req, res){
+  res.sendFile(path.join(__dirname, 'public/HW3.html'));
+});
+
+app.get('/', function(req, res){
+  res.sendFile(path.join(__dirname, 'public/about.html'));
+});
+
+app.get('/', function(req, res){
+  res.sendFile(path.join(__dirname, 'public/contact.html'));
+});
+
+app.get('/', function(req, res){
+  res.sendFile(path.join(__dirname, 'public/backlink.html'));
+});
+
 var server = app.listen(app.get('port'), function(){
-  console.log('Server listening on port 5000 ',app.get('port'));
- });
+  console.log('Server listening on port ',app.get('port'));
+});
