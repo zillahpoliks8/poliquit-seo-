47098ed303c17e98951bccac927db064f89c769b
ching poliks <poliquitzillahit1b@gmail.com>
2017-08-27 14:16:56 +0800

    organizing

--- server.js
+++ server.js
@@ -9,31 +9,35 @@ app.set('port', (process.env.PORT || 5000));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function(req, res){
-  res.sendFile(path.join(__dirname, 'public/index.html'));
+  res.sendFile(path.join(__dirname, 'views/index.html'));
 });
 
-app.get('/', function(req, res){
-  res.sendFile(path.join(__dirname, 'public/HW1.html'));
+app.get('/search_engine', function(req, res){
+  res.sendFile(path.join(__dirname, 'views/HW1.html'));
 });
 
-app.get('/', function(req, res){
-  res.sendFile(path.join(__dirname, 'public/HW2.html'));
+app.get('/SEO_vs_SEM', function(req, res){
+  res.sendFile(path.join(__dirname, 'views/HW2.html'));
 });
 
-app.get('/', function(req, res){
-  res.sendFile(path.join(__dirname, 'public/HW3.html'));
+app.get('/ON_page_vs_OFF_page', function(req, res){
+  res.sendFile(path.join(__dirname, 'views/HW3.html'));
 });
 
-app.get('/', function(req, res){
-  res.sendFile(path.join(__dirname, 'public/about.html'));
+app.get('/about', function(req, res){
+  res.sendFile(path.join(__dirname, 'views/about.html'));
 });
 
-app.get('/', function(req, res){
-  res.sendFile(path.join(__dirname, 'public/contact.html'));
+app.get('/contact', function(req, res){
+  res.sendFile(path.join(__dirname, 'views/contact.html'));
 });
 
-app.get('/', function(req, res){
-  res.sendFile(path.join(__dirname, 'public/backlink.html'));
+app.get('/backlink', function(req, res){
+  res.sendFile(path.join(__dirname, 'views/backlink.html'));
+});
+
+app.get('/resp', function(req, res){
+  res.sendFile(path.join(__dirname, 'views/resp.html'));
 });
 
 var server = app.listen(app.get('port'), function(){
